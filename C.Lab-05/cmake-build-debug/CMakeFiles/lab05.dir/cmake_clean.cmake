99d1c8f4018eb718a26421a0dcdc16755886e9a8
WotIzLove <32139991+WotIzLove@users.noreply.github.com>
2017-12-06 18:22:22 +0300

    Add files via upload

---
+++ C.Lab-05/cmake-build-debug/CMakeFiles/lab05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab05.dir/main.c.o"
+  "lab05.pdb"
+  "lab05"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
